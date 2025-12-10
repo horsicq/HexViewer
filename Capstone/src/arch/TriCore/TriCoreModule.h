5aa1f297327cedb479cf099bf578b78a534102dd
DevX-Cipher <dawgz1992.jp@gmail.com>
2025-12-10 14:24:32 +0800

    update
    removing local Capstone and fixing CMakeLists.txt

--- Capstone/src/arch/TriCore/TriCoreModule.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef CAPSTONE_TRICODEMODULE_H
-#define CAPSTONE_TRICODEMODULE_H
-
-cs_err TRICORE_global_init(cs_struct *ud);
-cs_err TRICORE_option(cs_struct *handle, cs_opt_type type, size_t value);
-
-#endif // CAPSTONE_TRICODEMODULE_H
