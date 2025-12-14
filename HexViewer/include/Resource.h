a934873a52dbe4ac6cf4ac4091233cdf5f288e04
DevX-Cipher <dawgz1992.jp@gmail.com>
2025-12-14 22:31:12 +0800

    Update Resource.h

--- HexViewer/include/Resource.h
+++ HexViewer/include/Resource.h
@@ -1,18 +1,18 @@
 #ifndef RESOURCE_H
 #define RESOURCE_H
 
-#define IDB_LOGO 1  // Logo bitmap/resource
-
-#endif // RESOURCE_H
+#define IDB_LOGO 1
 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 
 #define _APS_NO_MFC                 130
-#define _APS_NEXT_RESOURCE_VALUE    2    // Next available ID after your logo
+#define _APS_NEXT_RESOURCE_VALUE    2
 #define _APS_NEXT_COMMAND_VALUE     32771
 #define _APS_NEXT_CONTROL_VALUE     1000
 #define _APS_NEXT_SYMED_VALUE       110
 
 #endif
 #endif
+
+#endif // RESOURCE_H
